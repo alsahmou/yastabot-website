a707f5446bb662ea2ae3794ee0caa79ddfc65927
Ali Sahmoud <ali.sahmoud@mail.mcgill.ca>
2021-02-12 16:19:44 -0500

    why yastabot  page responsiveness fixed

--- src/components/WhyYastaBot.jsx
+++ src/components/WhyYastaBot.jsx
@@ -5,37 +5,23 @@ export class WhyYastabot extends Component {
     return (
       <div id="why-yastabot" className="text-center">
         <div className="container">
-          {/* <div className="row">
-            <div className="col-md-6 col-md-offset-4 section-title">
-              <h2>Why Yastabot?</h2>
-              <h4>Because setting reminders should be easy.</h4>
-            </div>
-          </div> */}
-          {/* <div>
-            <div className="row">
-              <div className="col-md-6"><img src="img/our-bot.png"></img></div>
-              <div className="col-md-6"><img src="img/our-bot.png"></img></div>
-            </div>
-            <div className="row">
-              <div className="col-md-6 text-center"><h3>Our bot</h3></div>
-              <div className="col-md-6 text-center"><h3>Other apps</h3></div>
-            </div>
-          </div> */}
           <div className="row">
             <h2>Why Yastabot?</h2>
           </div>
           <div className="row">
             <h4>Because setting reminders should be easy.</h4>
           </div>
-          <div className="row">
+          <div className="row img-container">
             <div className="row">
-              <div className="col-md-6"><img src="img/our-bot.png"></img><h3>Our bot</h3></div>
-              <div className="col-md-6"><img src="img/our-bot.png"></img><h3>Other apps</h3></div>
+              <div className="col-md-6">
+                <img src="img/our-bot.png"></img>
+                <h3>Our bot</h3>
+              </div>
+              <div className="col-md-6">
+                <img src="img/our-bot.png"></img>
+                <h3>Other apps</h3>
+              </div>
             </div>
-            {/* <div className="row">
-              <div className="col-md-6 text-center"><h3>Our bot</h3></div>
-              <div className="col-md-6 text-center"><h3>Other apps</h3></div>
-            </div> */}
           </div>
         </div>
       </div>
@@ -44,3 +30,25 @@ export class WhyYastabot extends Component {
 }
 
 export default WhyYastabot
+
+/* <div className="row">
+            <div className="col-md-6 col-md-offset-4 section-title">
+              <h2>Why Yastabot?</h2>
+              <h4>Because setting reminders should be easy.</h4>
+            </div>
+          </div> */
+          /* <div>
+            <div className="row">
+              <div className="col-md-6"><img src="img/our-bot.png"></img></div>
+              <div className="col-md-6"><img src="img/our-bot.png"></img></div>
+            </div>
+            <div className="row">
+              <div className="col-md-6 text-center"><h3>Our bot</h3></div>
+              <div className="col-md-6 text-center"><h3>Other apps</h3></div>
+            </div>
+          </div> */
+
+          /* <div className="row">
+              <div className="col-md-6 text-center"><h3>Our bot</h3></div>
+              <div className="col-md-6 text-center"><h3>Other apps</h3></div>
+            </div> */
