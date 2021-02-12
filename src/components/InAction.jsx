c22d41caef7ba162f574c9e2a24dc1e4a37a0d07
Ali Sahmoud <ali.sahmoud@mail.mcgill.ca>
2021-02-12 15:40:46 -0500

    in action page responsiveness fixed

--- src/components/InAction.jsx
+++ src/components/InAction.jsx
@@ -1,15 +1,24 @@
-import React, { Component } from "react";
+// import React, { Component } from "react";
 
-export class InAction extends Component {
-  render() {
-    return (
-      <div id="in-action" className="text-center">
-        <div className="container">
-          <div className="col-md-6 col-md-offset-3 section-title">
-            <h2>See how easy it is to set reminders!</h2>
-          </div>
-          <img src="img/in-action.png"></img>
-          {/* <div className="row">
+// export class InAction extends Component {
+//   render() {
+//     return (
+//       <div id="in-action" className="text-center">
+//         <div className="container">
+//           <div className="col-md-6 col-md-offset-3 section-title">
+//             <h2>See how easy it is to set reminders!</h2>
+//           </div>
+//           <img src="img/in-action.png"></img>
+//         </div>
+//       </div>
+//     );
+//   }
+// }
+
+// export default InAction;
+
+
+/* <div className="row">
             {this.props.data
               ? this.props.data.map((d,i) => (
                   <div  key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
@@ -20,7 +29,22 @@ export class InAction extends Component {
                   </div>
                 ))
               : "Loading..."}
-          </div> */}
+          </div> */
+
+
+          import React, { Component } from "react";
+
+export class InAction extends Component {
+  render() {
+    return (
+      <div id="in-action" className="text-center">
+        <div className="container">
+          <div className="row">
+            <h2>See how easy it is to set reminders!</h2>
+          </div>
+          <div className="row img-container">
+              <img src="img/in-action.png"></img>
+          </div>
         </div>
       </div>
     );
