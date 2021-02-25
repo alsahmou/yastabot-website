c50a60ad0def643301486a09a09789f51a5b1f67
Ali Sahmoud <ali.sahmoud@mail.mcgill.ca>
2021-02-24 20:30:49 -0500

    inAction section finalized

--- src/components/InAction.jsx
+++ src/components/InAction.jsx
@@ -1,36 +1,3 @@
-// import React, { Component } from "react";
-
-// export class InAction extends Component {
-//   render() {
-//     return (
-//       <div id="in-action" className="text-center">
-//         <div className="container">
-//           <div className="col-md-6 col-md-offset-3 section-title">
-//             <h2>See how easy it is to set reminders!</h2>
-//           </div>
-//           <img src="img/in-action.png"></img>
-//         </div>
-//       </div>
-//     );
-//   }
-// }
-
-// export default InAction;
-
-
-/* <div className="row">
-            {this.props.data
-              ? this.props.data.map((d,i) => (
-                  <div  key={`${d.title}-${i}`} className="col-xs-6 col-md-3">
-                    {" "}
-                    <i className={d.icon}></i>
-                    <h3>{d.title}</h3>
-                    <p>{d.text}</p>
-                  </div>
-                ))
-              : "Loading..."}
-          </div> */
-
 
 import React, { Component } from "react";
 
@@ -40,10 +7,10 @@ export class InAction extends Component {
       <div id="in-action" className="text-center">
         <div className="container">
           <div className="row img-container">
-            <h2>See how easy it is to set reminders!</h2>
+            <h2>{this.props.data ? this.props.data.title : "Loading"}</h2>
           </div>
           <div className="row img-container">
-              <img src="img/in-action.png"></img>
+              <img src={this.props.data ? this.props.data.img : "Loading"}></img>
           </div>
         </div>
       </div>
