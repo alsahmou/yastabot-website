4bcfa2a9e507dad0d46d6343240e1b361d44f443
Ali Sahmoud <ali.sahmoud@mail.mcgill.ca>
2021-02-24 20:40:13 -0500

    fixed the header section

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,61 +1,3 @@
-// import React, { Component } from "react";
-
-// export class Header extends Component {
-//   render() {
-//     return (
-//       <header id="header">
-//         <div id="intro">
-//             <div className="container">
-//               <div className="row intro-text text-center">
-//                 <div className="type-writer-container">
-//                   <p className="type-writer">/remind me in 20 minutes to workout</p>
-//                 </div>
-//               </div>
-//               <div className="row text-center">
-//                 <p className="type-writer">/remind me in 20 minutes to workout</p>
-//               </div>
-//               <div className="row text-center">
-//                 <p>Yastabot uses the latest AI and NLP technlogoy to make it easy for you to set reminders</p>
-//               </div>
-//               <div className="row text-center">
-//                 <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
-//                   <button className="btn btn-custom1 btn-lg">Get started</button>
-//                 </a>
-//               </div>
-//               <div className="row text-center">
-//                 <a href="https://google.com" target="_blank" rel="noopener noreferrer">
-//                   <button className="btn btn-custom2">Something</button>
-//                 </a>
-//                 <a href="https://telegram.com" target="_blank" rel="noopener noreferrer">
-//                   <button className="btn btn-custom2">Join our Channel</button>
-//                 </a>
-//               </div>
-//             </div>
-//         </div>
-//       </header>
-//     );
-//   }
-// }
-
-// export default Header;
-
-
-/* <div className="col-md-12 text-center intro-text">
-                  <h2>
-                    Insert typewriter here
-                  </h2>
-                  <p>
-                    {this.props.data ? this.props.data.paragraph : "Loading"}
-                  </p>
-                  <a
-                    href="#features"
-                    className="btn btn-custom btn-lg page-scroll"
-                  >
-                    Learn More
-                  </a>{" "}
-                </div> */
-
-
 import React, { Component } from "react";
 
 export class Header extends Component {
@@ -67,25 +9,25 @@ export class Header extends Component {
             <div className="row">
               <div className="type-writer-container">
                 <div className="row intro-text">
-                  <p className="type-writer">/remind me in 5m to eat</p>
+                  <p className="type-writer">{this.props.data ? this.props.data.typeWriter : "Loading"}</p>
                 </div>
               </div>
             </div>
             <div className="row text-center">
-              <p>Yastabot uses the latest AI and NLP technlogoy to make it easy for you to set reminders</p>
+              <p>{this.props.data ? this.props.data.text1 : "Loading"}</p>
             </div>
             <div className="row text-center">
               <div className="container">
-                <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+                <a href={this.props.data ? this.props.data.getStartedButton : "Loading"} target="_blank" rel="noopener noreferrer">
                   <button className="btn btn-custom1 btn-lg"><i className="fa fa-send"></i> Get started</button>
                 </a>
               </div>
             </div>
             <div className="row text-center">
-              <a href="https://google.com" target="_blank" rel="noopener noreferrer">
+              <a href={this.props.data ? this.props.data.button2 : "Loading"} target="_blank" rel="noopener noreferrer">
                 <button className="btn btn-custom2">Something</button>
               </a>
-              <a href="https://telegram.com" target="_blank" rel="noopener noreferrer">
+              <a href={this.props.data ? this.props.data.button3 : "Loading"} target="_blank" rel="noopener noreferrer">
                 <button className="btn btn-custom2">Join our Channel</button>
               </a>
             </div>
